4c1052f320b943e3c301ab1d5823208e873694ad
marco13-moo <210023993+marco13-moo@users.noreply.github.com>
2026-02-02 13:47:47 +0000

    fixes

--- control-plane/internal/api/environment_status.go
+++ control-plane/internal/api/environment_status.go
@@ -2,7 +2,7 @@ package api
 
 import "net/http"
 
-func (h *Handler) GetEnvironmentStatus(w http.ResponseWriter, r *http.Request) {
+func (h *Handlers) GetEnvironmentStatus(w http.ResponseWriter, r *http.Request) {
 	// 1. Resolve environment
 	// 2. Fetch WorkflowReference
 	// 3. Call orchestrator.GetWorkflowStatus
