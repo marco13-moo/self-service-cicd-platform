53052998f4d311f2cad6ce724437c972d4cfd186
marco13-moo <marco.donlic13@outlook.com>
2026-02-12 08:02:58 +0000

    phase 7 work

--- control-plane/internal/orchestrator/environment.go
+++ control-plane/internal/orchestrator/environment.go
@@ -3,6 +3,8 @@ package orchestrator
 import (
 	"context"
 	"time"
+
+	wf "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 )
 
 //
@@ -20,9 +22,17 @@ type EnvironmentSpec struct {
 
 // WorkflowReference is a stable identifier for an execution-plane workflow.
 // It is immutable and read-only.
+/*
 type WorkflowReference struct {
 	Name        string
 	Namespace   string
+	Template    string
+	SubmittedAt time.Time
+}*/
+type WorkflowReference struct {
+	Name        string `json:"name"`
+	Namespace   string `json:"namespace"`
+	UID         string `json:"uid"`
 	Template    string
 	SubmittedAt time.Time
 }
@@ -43,11 +53,13 @@ type Environment struct {
 
 // WorkflowStatusView is derived execution data.
 // It is queried on demand and never stored.
+/*
 type WorkflowStatusView struct {
 	Phase      string
 	StartedAt  *time.Time
 	FinishedAt *time.Time
 }
+*/ //using wf "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 
 //
 // ----- ORCHESTRATOR CONTRACT -----
@@ -61,11 +73,16 @@ type EnvironmentOrchestrator interface {
 	Create(ctx context.Context, spec EnvironmentSpec) (*Environment, error)
 
 	// Destroy submits intent to destroy an environment.
-	Destroy(ctx context.Context, name string) (*WorkflowReference, error)
+	//Destroy(ctx context.Context, name string) (*WorkflowReference, error)
+	Destroy(ctx context.Context, name string, service string) (*WorkflowReference, error)
 
 	// GetCreateStatus returns the current status of the create workflow.
-	GetCreateStatus(ctx context.Context, env *Environment) (*WorkflowStatusView, error)
+	//GetCreateStatus(ctx context.Context, env *Environment) (*WorkflowStatusView, error)
 
 	// GetTTLStatus returns the current status of the TTL cleanup workflow.
-	GetTTLStatus(ctx context.Context, env *Environment) (*WorkflowStatusView, error)
+	//GetTTLStatus(ctx context.Context, env *Environment) (*WorkflowStatusView, error)
+
+	GetCreateStatus(ctx context.Context, env *Environment) (*wf.WorkflowStatus, error)
+
+	GetTTLStatus(ctx context.Context, env *Environment) (*wf.WorkflowStatus, error)
 }
